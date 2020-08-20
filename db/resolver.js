60a7ae72ae08f71b0149d8b66a9163aad4332b22
lu15v <lu15bytes@gmail.com>
2020-08-19 20:43:43 -0700

    DB: newUser mutation created

--- db/resolver.js
+++ db/resolver.js
@@ -1,7 +1,34 @@
+const User = require('../models/User');
+const bcryptjs = require('bcryptjs');
+
 //Resolver
 const resolvers  = {
     Query: {
         getCourse: () => "something"
+    },
+    Mutation :{
+        newUser: async (_, {input}) => {
+            
+            const {email, password} = input;
+            //Check if the user exists.
+            const userExists = await User.findOne({email});
+            if(userExists){
+                throw new Error('User already registered')
+            }
+            //Passwords Hashing
+            //const salt = await bcryptjs.getSalt(10);
+            input.password = await bcryptjs.hash(password, bcryptjs.genSaltSync(10));
+            //save it to the db.
+            try{
+                const user = new User(input);
+                user.save();
+                return user;
+            }
+            catch(error){
+                console.log(error)
+            }
+
+        }
     }
 }
 
--- db/schema.js
+++ db/schema.js
@@ -2,8 +2,28 @@ const {gql} = require('apollo-server');
 
 //Schema
 const typeDefs = gql`
+    type User {
+        id: ID
+        name: String
+        lastName: String
+        email: String
+        created: String        
+    }
+    
+    input UserInput {
+        name: String!
+        lastName: String!
+        email: String!
+        password: String!
+    }
+
     type Query {
         getCourse: String
+    }
+    
+    type Mutation {
+        newUser(input: UserInput) : User
+
     }
 `;
 
