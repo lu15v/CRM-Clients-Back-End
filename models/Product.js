8d2352ed7de1a88cfa6cf856e3adbabd98119572
lu15v <lu15bytes@gmail.com>
2020-08-20 23:29:52 -0700

    DB: Product model created

---
+++ models/Product.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const ProductsSchema = mongoose.Schema({
+    name:  {
+        type: String,
+        required: true,
+        trim: true
+    },
+    stock: {
+        type: Number,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        trim: true
+    },
+    created: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+
+
+module.exports = mongoose.model("Product", ProductsSchema);
